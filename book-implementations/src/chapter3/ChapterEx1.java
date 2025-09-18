0fa99dc397e3db1f0dfa0e1240abb1020da90990
Taeyoung Hwang <taeyounghwang99@gmail.com>
2025-09-18 19:05:59 -0400

    Re-added Chapter 3 Exercise 1 and 2

--- book-implementations/src/chapter3/ChapterEx1.java
+++ book-implementations/src/chapter3/ChapterEx1.java
@@ -1,4 +1,11 @@
 package chapter3;
 
 public class ChapterEx1 {
+    public static void main(String[] args) {
+        double weightDino1 = 140.34;
+        double weightDino2 = 109.5;
+
+        double calculateAvg = (weightDino1 + weightDino2) / 2;
+        System.out.println("The average weight of two dinos are " +  calculateAvg);
+    }
 }
--- book-implementations/src/chapter3/ChapterEx2.java
+++ book-implementations/src/chapter3/ChapterEx2.java
@@ -1,4 +1,13 @@
 package chapter3;
 
 public class ChapterEx2 {
+    public static void main(String[] args) {
+        //Suppose Dinosaurs need 0.4kg of food per 1kg of their body weight
+
+        double dinoWeight = 152.12;
+        double calculateFood =  dinoWeight * 0.4;
+
+        System.out.println("The dinosaur's weight is " + dinoWeight + " and it needs " + calculateFood
+        + "kg worth of food");
+    }
 }
